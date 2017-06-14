652c81ffd593a8272f996ed81e6f956873ed06ec
Viktoriia Chernyshuk <vickierose.ch@gmail.com>
2017-06-14 18:13:07 +0300

    created page components for "My movies collections", "My favorite movies" and "Settings" and routes to them

--- js/containers/routes.js
+++ js/containers/routes.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import App from './app';
 import MainPage from '../components/mainPage';
+import MoviesCollectionsPage from '../components/moviesCollectionsPage';
+import FavoriteMoviesPage from '../components/favoriteMoviesPage';
+import SettingsPage from '../components/settingsPage';
 
 const Routes = () => {
   return (
     <div>
       <Route component={ App } />
       <Switch>
-        <Route path='/' component={ MainPage } />
+        <Route exact path='/' component={ MainPage } />
+        <Route path='/my-movies-collections' component={ MoviesCollectionsPage } />
+        <Route path='/my-favorite-movies' component={ FavoriteMoviesPage } />
+        <Route path='/settings' component={ SettingsPage } />
       </Switch>
     </div>
   );
