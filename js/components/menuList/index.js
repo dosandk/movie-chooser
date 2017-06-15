d5bb0dd0b6a7332b63f56fddc5a477053dbba38b
Viktoriia Chernyshuk <vickierose.ch@gmail.com>
2017-06-15 17:37:27 +0300

    added icons

--- js/components/menuList/index.js
+++ js/components/menuList/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import MenuItem from '../menuItem';
 import { List } from 'material-ui/List';
+import ActionHome from 'material-ui/svg-icons/action/home';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import ActionSettings from 'material-ui/svg-icons/action/settings';
+import ImageCollections from 'material-ui/svg-icons/image/collections';
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
 
 const MenuList = () => {
   return (
     <List>
-      <MenuItem linkTo = '/' text = 'Home'/>
-      <MenuItem linkTo = '/my-movies-collections' text = 'My Movies Collections'/>
-      <MenuItem linkTo = '/my-favorite-movies' text = 'My favorite movies'/>
-      <MenuItem linkTo = '/settings' text = 'Settings'/>
+      <NavigationMenu />
+      <MenuItem linkTo = '/' text = 'Home' icon = {<ActionHome />}/>
+      <MenuItem linkTo = '/my-movies-collections' text = 'My Movies Collections' icon = {<ImageCollections />}/>
+      <MenuItem linkTo = '/my-favorite-movies' text = 'My favorite movies' icon = {<ActionGrade />}/>
+      <MenuItem linkTo = '/settings' text = 'Settings' icon = {<ActionSettings />}/>
     </List>
   );
 };
